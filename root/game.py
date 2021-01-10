b1ecc61a52b399a8e9a15d00d25fe6c79ff4ba62
AndyOpris <38504992+AndyOpris@users.noreply.github.com>
2021-01-10 03:08:05 +0200

    Create game.py

---
+++ root/game.py
@@ -0,0 +1,18 @@
+Search:
+		def SetHPTargetBoard(self, vid, hpPercentage, iMinHP, iMaxHP):
+			if vid != self.targetBoard.GetTargetVID():
+				self.targetBoard.ResetTargetBoard()
+				self.targetBoard.SetEnemyVID(vid)
+			
+			self.targetBoard.SetHP(hpPercentage, iMinHP, iMaxHP)
+			self.targetBoard.Show()
+      
+Chnage in:
+		def SetHPTargetBoard(self, vid, hpPercentage, iMinHP, iMaxHP, bLevel=0):
+			if vid != self.targetBoard.GetTargetVID():
+				self.targetBoard.ResetTargetBoard()
+				self.targetBoard.SetEnemyVID(vid, bLevel)
+			
+			self.targetBoard.SetHP(hpPercentage, iMinHP, iMaxHP)
+			self.targetBoard.SetElementImage(bElement)
+			self.targetBoard.Show()
